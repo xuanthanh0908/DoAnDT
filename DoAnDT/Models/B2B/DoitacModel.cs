670bb9027dd35e5b78317e685d3e6c3e1d0716b7
Nguyễn Xuân Thành <91194311+xuanthanh0908@users.noreply.github.com>
2022-01-15 23:18:19 +0700

    Add files via upload

---
+++ DoAnDT/Models/B2B/DoitacModel.cs
@@ -0,0 +1,21 @@
+﻿using DoAnDT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnDT.Models
+{
+    public class DoitacModel
+    {
+        public List<NhaCungCap> LayDoitac()
+        {
+            using(DBDTConnect db = new DBDTConnect())
+            {
+                var ds = (from p in db.NhaCungCaps where p.Net_user == null select p).ToList();
+                //var ds = (from p in db.NhaCungCaps select p).ToList();
+                return ds;
+            }
+        }
+    }
+}
---
+++ DoAnDT/Models/B2B/SanphamcanmuaModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DoAnDT.Models
+{
+    public class SanphamcanmuaModel
+    {
+        DBDTConnect db = new DBDTConnect();
+        public List<Sanphamcanmua> getDS(int index,int count)
+        {
+            using(DBDTConnect db = new DBDTConnect())
+            {
+                var ds =  db.Sanphamcanmuas.OrderBy(m=>m.Ngaydang).Skip(index).Take(count).ToList();
+                return ds;
+            }
+        }
+        public SanPham getSP(string maSP)
+        {
+            using (DBDTConnect db = new DBDTConnect())
+            {
+                return (from p in db.SanPhams where p.MaSP == maSP select p).FirstOrDefault();
+            }
+        }
+        public Sanphamcanmua getSanphamcanmua(int ID)
+        {
+            using(DBDTConnect db = new DBDTConnect())
+            {
+                Sanphamcanmua var = (from p in db.Sanphamcanmuas where p.ID == ID select p).FirstOrDefault();
+                return var;
+            }
+        }
+        internal IQueryable<Sanphamcanmua> TimSPCM(string key)
+        {
+            IQueryable<Sanphamcanmua> lst = db.Sanphamcanmuas;
+            if (!string.IsNullOrEmpty(key))
+                lst = lst.Where(m => m.SanPham.TenSP.Contains(key));
+            return lst;
+        }
+
+        internal void DeleteSPCM(int id)
+        {
+            Sanphamcanmua loai = db.Sanphamcanmuas.Find(id);
+            db.Sanphamcanmuas.Remove(loai);
+            db.SaveChanges();
+        }
+
+        internal string ThemSPCM(Sanphamcanmua loai)
+        {
+            loai.Ngaydang = DateTime.Today;
+            db.Sanphamcanmuas.Add(loai);
+            db.SaveChanges();
+            return null;
+        }
+
+        internal void EditSPCM(SanPhamCanMuaEdit loai)
+        {
+            Sanphamcanmua lsp = db.Sanphamcanmuas.Find(loai.ID);
+            lsp.Soluong = loai.Soluong;
+            lsp.Ngayketthuc = loai.Ngayketthuc;
+            lsp.Mota = loai.Mota;
+            db.Entry(lsp).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
