ef9e243f905a930cd220d277a5f568310d4e7715
Nguyễn Xuân Thành <91194311+xuanthanh0908@users.noreply.github.com>
2022-01-15 23:17:48 +0700

    Create Classsupport.cs

---
+++ DoAnDT/Models/B2B/Classsupport.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DoAnDT.Models
+{
+    public class Classsupport
+    {
+    }
+    public class Hopdong
+    {
+        public string order_id { get; set; }
+        public string product_id { get; set; }
+        public string product_name { get; set; }
+        public string product_quantity { get; set; }
+        public DateTime product_date { get; set; }
+        public string supplier_key { get; set; }
+    }
+    public class Token
+    {
+        public string access_token { get; set; }
+    }
+}
