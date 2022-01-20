385d0d7757fb7cbfe44f13c449cbaacbac69e365
nguyen xuan thanh <hd13112011@gmail.com>
2022-01-20 16:04:56 +0700

    Update backend(5) 20-01-21

--- DoAnDT/Controllers/ThongKeController.cs
+++ DoAnDT/Controllers/ThongKeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAnDT.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,35 @@ namespace DoAnDT.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ThongKeDoanhThu()
+        {
+            return View();
+        }
+
+        public ActionResult ThongKeDTTheoTG(DateTime? froms, DateTime? tos)
+        {
+            if (froms == null)
+                froms = DateTime.Today.AddMonths(-1);
+            if (tos == null)
+                tos = DateTime.Today;
+            ViewBag.froms = froms.Value.ToShortDateString();
+            ViewBag.tos = tos.Value.ToShortDateString();
+            DonhangKHModel donhang = new DonhangKHModel();
+            return PartialView("TheoTime", donhang.ThongKeDoanhThu(froms, tos).ToList());
+        }
+
+        public ActionResult ThongKeTiTrong(DateTime? froms, DateTime? tos)
+        {
+            if (froms == null)
+                froms = DateTime.Today.AddMonths(-1);
+            if (tos == null)
+                tos = DateTime.Today;
+            ViewBag.froms = froms.Value.ToShortDateString();
+            ViewBag.tos = tos.Value.ToShortDateString();
+            DonhangKHModel donhang = new DonhangKHModel();
+            return PartialView("TheoTiTrong", donhang.ThongKeTiTrong(froms, tos).ToList());
         }
     }
 }
